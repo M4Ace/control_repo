d579d944b44c7eb25dc7f5b0cf081ef90eb0d92e
M4Ace <81255535+M4Ace@users.noreply.github.com>
2021-06-11 13:16:34 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,17 +1,21 @@
 node default {
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
 node 'master.puppet.vm' {
   include role::master_server
-  file { '/root/README':
+  file {'/root/README':
     ensure => file,
-    content => "Welcome to ${fqdn}\n",
-   }
-} 
-
-node /^web/ {
+    content => “Welcome to ${fqdn}”,
+    owner => 'root',
+  }
+}
+node /^web/ { 
   include role::app_server
 }
-
 node /^db/ {
   include role::db_server
 }
